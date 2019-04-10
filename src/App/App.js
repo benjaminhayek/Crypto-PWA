ecc6593945b7c811b8aca726f3db3cc617056f5b
bhayek12@gmail.com <bhayek12@gmail.com>
2019-04-10 14:59:13 -0600

    Change file structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Today from './Today/Today'
-import History from './History/History'
+import Today from '../Today/Today'
+import History from '../History/History'
 
 class App extends Component {
   render() {
